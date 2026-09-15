0a277e3532c146d3b1dd1c64f79393e95b172d7a
S7venYoung <S7venYoung@users.noreply.github.com>
2026-09-15 13:52:08 +0800

    host-status: add daily forecast fields

--- host-status/include/zmk/host_status.h
+++ host-status/include/zmk/host_status.h
@@ -8,6 +8,9 @@ struct zmk_host_status {
     int16_t temperature_deci_c;
     uint8_t weather_code;
     uint32_t observed_at;
+    int16_t high_temperature_deci_c;
+    int16_t low_temperature_deci_c;
+    uint8_t rain_probability;
 };
 
 struct zmk_host_status zmk_host_status_get(void);
