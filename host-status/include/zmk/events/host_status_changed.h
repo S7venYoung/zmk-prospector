7b37dffda94d3638ab9b353640375947426f0921
S7venYoung <S7venYoung@users.noreply.github.com>
2026-09-15 13:52:18 +0800

    host-status: expose daily forecast event fields

--- host-status/include/zmk/events/host_status_changed.h
+++ host-status/include/zmk/events/host_status_changed.h
@@ -8,6 +8,9 @@ struct zmk_host_status_changed {
     int16_t temperature_deci_c;
     uint8_t weather_code;
     uint32_t observed_at;
+    int16_t high_temperature_deci_c;
+    int16_t low_temperature_deci_c;
+    uint8_t rain_probability;
 };
 
 ZMK_EVENT_DECLARE(zmk_host_status_changed);
